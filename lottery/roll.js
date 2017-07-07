6ba47a33906e674bd2c784fac9ae4f7517186a55
huyuan <422018883@qq.com>
2017-07-07 12:50:15 +0800

    定时器修改

--- lottery/roll.js
+++ lottery/roll.js
@@ -7,19 +7,28 @@ $(function(){
         topDefault = 0, //top的初始值
         speed = 30, //滚动的速度
         card = $('.lottery-card'),
-        time;
+        timer = null;
 
     rollLis.clone(true).appendTo(rollUl); //复制所有的li
-    timer = setInterval(
-        function(){
-            rollUl[0].style.top = (topDefault--) + 'px'; // top值递减1
 
+    function hover(onOff) {
+        clearInterval(timer); //启动定时器之前，清除以前的定时器，否则会越来越快
+        timer = setInterval(function(){
+            rollUl[0].style.top = (topDefault--) + 'px'; // top值递减1
             if(topDefault == -rollUl.height()/2){
                 topDefault = 0; //当top值为ul高度的一半的时候，top值设为0；保证无缝滚动
             }
-        },speed
-    )
+        },speed);
+        if(!onOff){
+            clearInterval(timer);
+        }
+    }
+    //true 表示开启定时器，flase表示清除定时器
+    hover(true);
     card.mouseover(function(){
-        clearInterval(timer); //清除定时器
+        hover(false);
+    });
+    card.mouseout(function(){
+        hover(true);
     });
 });
