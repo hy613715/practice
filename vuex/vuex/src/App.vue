2ef199b2b8dff5b6708f6c2a2d327b9b69c00361
huyuan <422018883@qq.com>
2017-07-10 19:46:54 +0800

    count

--- vuex/vuex/src/App.vue
+++ vuex/vuex/src/App.vue
@@ -1,23 +1,51 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <count></count>
+    </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import Vuex from 'vuex'
+    import Count from './components/Count.vue'
+
+    Vue.use(Vue)
+    Vue.use(Vuex)
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment: state => state.count++,
+        decrement: state => state.count--
+    }
+})
+
+new Vue({
+    el: '#app',
+    computed: {
+        count () {
+            return store.state.count
+        }
+    },
+    methods: {
+        increment () {
+        store.commit('increment')
+        },
+        decrement () {
+            store.commit('decrement')
+        }
+    }
+})
 export default {
-  name: 'app'
+    components: {
+        'count': Count
+    }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ vuex/vuex/src/components/Count.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>{{count}}</p>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </p>
+    </div>
+</template>
+<script>
+
+</script>
